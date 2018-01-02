4c3fa9ab835a061703076a267d16201ee5bb842b
victor <zwdong1994@gmail.com>
2018-01-02 21:49:00 +0800

    Add a EaD evaluation sample that are uses fiu trace.

---
+++ test_all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Program:
+#       This program is to evaluate the raidmeter performance in different traces.
+# History:
+# 2018.1.2  Vitor Zhu       First release.
+
+./single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace ;
+resultname=homes.traditional capacity=100 devicename=/dev/nvme0n1
+
+./single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace ;
+resultname=homes.EaD capacity=100 devicename=/dev/nvme0n1
+
+./single_evaluation.sh dedupschems=0 tracetype=0 tracename=web_500000.trace ;
+resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
+
+./single_evaluation.sh dedupschems=1 tracetype=0 tracename=web_500000.trace ;
+resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
+
+./single_evaluation.sh dedupschems=0 tracetype=1 tracename=mail_500000.trace ;
+resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
+
+./single_evaluation.sh dedupschems=1 tracetype=1 tracename=mail_500000.trace ;
+resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
