374fe06359f98b98c6484b9e76d8a1ad63e81a6f
victor <zwdong1994@gmail.com>
2018-01-02 21:51:52 +0800

    Add a EaD evaluation sample that are uses fiu trace.

--- test_all.sh
+++ test_all.sh
@@ -4,20 +4,20 @@
 # History:
 # 2018.1.2  Vitor Zhu       First release.
 
-./single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace ;
 resultname=homes.traditional capacity=100 devicename=/dev/nvme0n1
 
-./single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace ;
 resultname=homes.EaD capacity=100 devicename=/dev/nvme0n1
 
-./single_evaluation.sh dedupschems=0 tracetype=0 tracename=web_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=0 tracetype=0 tracename=web_500000.trace ;
 resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
 
-./single_evaluation.sh dedupschems=1 tracetype=0 tracename=web_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=1 tracetype=0 tracename=web_500000.trace ;
 resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
 
-./single_evaluation.sh dedupschems=0 tracetype=1 tracename=mail_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=0 tracetype=1 tracename=mail_500000.trace ;
 resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
 
-./single_evaluation.sh dedupschems=1 tracetype=1 tracename=mail_500000.trace ;
+sudo ./single_evaluation.sh dedupschems=1 tracetype=1 tracename=mail_500000.trace ;
 resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
