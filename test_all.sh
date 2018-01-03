aea8e566163d215cea5acd8711f17709f36a87b3
victor <zwdong1994@gmail.com>
2018-01-03 16:05:51 +0800

    Fix a bug in the test_all.sh.

--- test_all.sh
+++ test_all.sh
@@ -8,10 +8,10 @@ sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.
 
 sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=homes.EaD capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=0 tracename=web_500000.trace resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=web_500000.trace resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=0 tracename=web_500000.trace resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=web_500000.trace resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=mail_500000.trace resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=mail_500000.trace resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=mail_500000.trace resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
+sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=mail_500000.trace resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
