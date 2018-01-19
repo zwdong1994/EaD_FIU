b368150d4914c45158bdf1e805235b762125886d
victor <zwdong1994@gmail.com>
2018-01-19 22:27:59 +0800

    Add sample test.

--- test_all.sh
+++ test_all.sh
@@ -4,14 +4,17 @@
 # History:
 # 2018.1.2  Vitor Zhu       First release.
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace resultname=homes.traditional capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace resultname=homes.traditional capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=homes.EaD capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=homes.EaD capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=web_500000.trace resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=web_500000.trace resultname=web.traditional capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=web_500000.trace resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=web_500000.trace resultname=web.EaD capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=mail_500000.trace resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=mail_500000.trace resultname=mail.traditional capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=mail_500000.trace resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=mail_500000.trace resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
+
+sudo bash single_evaluation.sh dedupschems=2 tracetype=1 tracename=homes_500000.trace resultname=homes.sample capacity=100 devicename=/dev/nvme0n1
+
