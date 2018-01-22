01adb1e55b2314e111b5d9a97249c9d3cad85635
victor <zwdong1994@gmail.com>
2018-01-22 21:19:34 +0800

    Add sample test with mail and web workload in the test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -16,5 +16,8 @@
 
 #sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=mail_500000.trace resultname=mail.EaD capacity=100 devicename=/dev/nvme0n1
 
-sudo bash single_evaluation.sh dedupschems=2 tracetype=1 tracename=homes_500000.trace resultname=homes.sample capacity=100 devicename=/dev/nvme0n1
+#sudo bash single_evaluation.sh dedupschems=2 tracetype=1 tracename=homes_500000.trace resultname=homes.sample capacity=100 devicename=/dev/nvme0n1
 
+sudo bash single_evaluation.sh dedupschems=2 tracetype=2 tracename=web_500000.trace resultname=web.sample capacity=100 devicename=/dev/nvme0n1
+
+sudo bash single_evaluation.sh dedupschems=2 tracetype=2 tracename=mail_500000.trace resultname=mail.sample capacity=100 devicename=/dev/nvme0n1
