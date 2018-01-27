b2787a9add39ae8ed3b92cc3827f213c97ad3d39
victor <zwdong1994@gmail.com>
2018-01-27 14:19:52 +0800

    Add a time output.

--- raidmeter_f.cpp
+++ raidmeter_f.cpp
@@ -990,6 +990,9 @@ void deal_by_num()
 	}
 	for (i=0; i<total; i++)
 	{
+        std::cout << "start time: " << my_time[i].start_time << " end time: " <<
+                  my_time[i].end_time << "elpsd time: " << my_time[i].elpsd_time <<
+                  std::endl;
 		time_use[i/deal_num]+=my_time[i].elpsd_time;
 		num[i/deal_num]++;
 		block_count[i/deal_num]+=trace[i].blkcount;
