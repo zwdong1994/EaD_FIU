5a1eccce2008540096902ac12d61156eb28c8b90
victor <zwdong1994@gmail.com>
2018-01-27 22:15:12 +0800

    Modify the sequence of test samples.

--- test_all.sh
+++ test_all.sh
@@ -10,13 +10,14 @@ if [ ! -d "$OUTPUT_FOLDER" ]; then
     mkdir "$OUTPUT_FOLDER"
 fi
 
-#sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.traditional capacity=100 devicename=/dev/nvme0n1
-
-sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.EaD capacity=100 devicename=/dev/nvme0n1
 
 #sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=web_500000.trace resultname=${OUTPUT_FOLDER}/web.traditional capacity=100 devicename=/dev/nvme0n1
 
 sudo bash single_evaluation.sh dedupschems=1 tracetype=2 tracename=web_500000.trace resultname=${OUTPUT_FOLDER}/web.EaD capacity=100 devicename=/dev/nvme0n1
+
+#sudo bash single_evaluation.sh dedupschems=0 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.traditional capacity=100 devicename=/dev/nvme0n1
+
+sudo bash single_evaluation.sh dedupschems=1 tracetype=1 tracename=homes_500000.trace resultname=${OUTPUT_FOLDER}/homes.EaD capacity=100 devicename=/dev/nvme0n1
 
 #sudo bash single_evaluation.sh dedupschems=0 tracetype=2 tracename=mail_500000.trace resultname=${OUTPUT_FOLDER}/mail.traditional capacity=100 devicename=/dev/nvme0n1
 
